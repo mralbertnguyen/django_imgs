224a9ea90aefed047ea663d13cebded4e7b934a0
albertnguyen <ndttam.dev@gmail.com>
2019-05-22 11:59:17 +0700

    Fix and run django example

--- app/bmicalculator_api/settings.py
+++ app/bmicalculator_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wpcsuyovo_tsv&39nrm@iohy&s5@(3+3qk6mkp8qfe@i@4quk8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'bmicalculator_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
